7ea283c482d0e37597b827813d0c58a9635bdd7e
John Absher <john.a.absher@gmail.com>
2025-04-10 08:49:01 -0400

    initial commit of (mostly) working version

---
+++ KerbalismLabExperimentsExpandedContinued/src/LabExperimentPackage.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+namespace KerbalismLabExperimentsExpandedContinued
+{
+    public class BaseLabExperimentPackage : PartModule
+    {
+        public override string GetInfo()
+        {
+            return $"Class Name: {GetType().Name}";
+        }
+    }
+
+    public class DrillLabExperimentPackage : BaseLabExperimentPackage
+    {
+    }
+
+    public class HighEnergyLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+
+    public class MultispectralLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+
+    public class SurfaceLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+
+    public class TelescopeLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+
+    public class VisualLabExperimentPackage : BaseLabExperimentPackage
+    {
+  
+    }
+
+    public class MaterialsLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+
+    public class AtmosphereLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+
+    public class FluidLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+
+    public class GravityLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+
+    public class PlasmaLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+
+    public class BiologyLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+
+    public class SpectrometerLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+
+    public class MagnetometerLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+    public class RadarLabExperimentPackage : BaseLabExperimentPackage
+    {
+
+    }
+}
